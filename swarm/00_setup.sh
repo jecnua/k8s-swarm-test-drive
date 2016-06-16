2789b057ac10645db5ee6bca1fb14f56a5b22afd
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2016-06-16 16:48:59 +0100

    Added first tests with swarm

---
+++ swarm/00_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SERVER_NAME='swarm-01'
+
+docker-machine create --driver amazonec2 \
+  --amazonec2-ami ami-ab0210c7  \
+  --amazonec2-region eu-central-1 \
+  $SERVER_NAME
---
+++ swarm/99_tear_down.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+SERVER_NAME='swarm-01'
+
+docker-machine kill $SERVER_NAME
