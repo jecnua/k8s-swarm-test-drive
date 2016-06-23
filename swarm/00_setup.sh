e25ab5e49062a39c0700ef1ae3d26fdf5202df8e
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2016-06-23 11:25:53 +0100

    Updated with new beta

--- swarm/00_setup.sh
+++ swarm/00_setup.sh
@@ -2,7 +2,8 @@
 
 SERVER_NAME='swarm-01'
 
-docker-machine create --driver amazonec2 \
+docker-machine create \
+  --driver amazonec2 \
   --amazonec2-ami ami-ab0210c7  \
   --amazonec2-region eu-central-1 \
   $SERVER_NAME
