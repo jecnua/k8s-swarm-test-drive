8eb06f220d176d462f5b634e66c77623070d7350
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2018-03-28 16:42:42 +0100

    Fixed go provisioner

--- k8s/local_mac/provision-go.sh
+++ k8s/local_mac/provision-go.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-# Source https://www.samclarke.com/installing-go-1-9-on-ubuntu-16-04/
+# Source: https://www.samclarke.com/installing-go-1-9-on-ubuntu-16-04/
+# Then modified
 
 sudo su
 add-apt-repository -y ppa:gophers/archive
 apt update
 apt-get install -y golang-1.9-go
-echo "export PATH=\$PATH:/usr/lib/go-1.9/bin:/root/bin" >> /root/.profile
-echo "export GOPATH=/home/root" >> /root/.profile
+echo "export PATH=\$PATH:/usr/lib/go-1.9/bin:/root/bin" >> /root/.bashrc
+echo "export GOPATH=/root" >> /root/.bashrc
 # shellcheck disable=SC1091
-source /root/.profile
+source /root/.bashrc
 go get github.com/kubernetes-incubator/cri-tools/cmd/crictl
