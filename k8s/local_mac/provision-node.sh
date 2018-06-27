1b5b480f08fb3d1171ea714c7db8740d7a4266cc
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2018-06-27 15:39:44 +0100

    updated k8s to 1.10.5

--- k8s/local_mac/provision-node.sh
+++ k8s/local_mac/provision-node.sh
@@ -2,7 +2,7 @@
 
 sudo su
 
-VERSION='1.10.4'
+VERSION='1.10.5'
 
 apt-get update && apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
