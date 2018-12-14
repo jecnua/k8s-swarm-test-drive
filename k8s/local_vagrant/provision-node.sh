4ea22e81f41d3a465ee4042fd3cfb45a4c7de469
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2018-12-14 11:44:09 +0000

    Removed kubernetes-cni package since it is installed as dep. Updating to version 1.13.1

--- k8s/local_vagrant/provision-node.sh
+++ k8s/local_vagrant/provision-node.sh
@@ -2,7 +2,7 @@
 
 sudo su
 
-VERSION='1.13.0'
+VERSION='1.13.1'
 
 apt-get update && apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
@@ -19,9 +19,9 @@ apt-get clean
 locale-gen en_GB.UTF-8 # Will fix the warning when logging to the box
 
 apt-get install -y docker.io apt-transport-https awscli jq curl nfs-common
-apt-get install -y kubelet=$VERSION-00 kubeadm=$VERSION-00 kubectl=$VERSION-00 kubernetes-cni=0.6.0-00
+apt-get install -y kubelet=$VERSION-00 kubeadm=$VERSION-00 kubectl=$VERSION-00
 # Hold these packages back so that we don't accidentally upgrade them.
-apt-mark hold kubelet=$VERSION-00 kubeadm=$VERSION-00 kubectl=$VERSION-00 kubernetes-cni=0.6.0-00
+apt-mark hold kubelet=$VERSION-00 kubeadm=$VERSION-00 kubectl=$VERSION-00
 
 # https://github.com/kubernetes/kubernetes/issues/44750
 iptables -F
